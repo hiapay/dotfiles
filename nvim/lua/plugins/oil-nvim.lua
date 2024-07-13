deabaee278122fdb29024eacaf6fe0149ae1bc13
hiapay <hiapay@gmail.com>
2024-07-13 11:38:56 +0900

    feat(oil): map -

--- nvim/lua/plugins/oil-nvim.lua
+++ nvim/lua/plugins/oil-nvim.lua
@@ -7,7 +7,7 @@ return {
       opts = {
         mappings = {
           n = {
-            ["<Leader>O"] = { function() require("oil").open() end, desc = "Open folder in Oil" },
+            ["-"] = { function() require("oil").open() end, desc = "Open folder in Oil" },
           },
         },
         autocmds = {
