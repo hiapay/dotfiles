f84cb39e2c291a4b6bb315e44f95e0731217a03a
hiapay <hiapay@gmail.com>
2024-07-14 16:45:49 +0900

    feat(neovim): add catppuccin

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,89 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  ---@type CatppuccinOptions
+  opts = {
+    {
+      flavour = "macchiato", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = false, -- disables setting the background color.
+      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      no_italic = false, -- Force no italic
+      no_bold = false, -- Force no bold
+      no_underline = false, -- Force no underline
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      default_integrations = true,
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dap = true,
+        dap_ui = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = true,
+        markdown = true,
+        mason = true,
+        native_lsp = {
+          enabled = true
+        },
+        neotree = true,
+        notify = true,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        treesitter = true,
+        ts_rainbow = false,
+        ufo = true,
+        which_key = true,
+        window_picker = true,
+      },
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          return require("astrocore").extend_tbl(opts, {
+            highlights = require("catppuccin.groups.integrations.bufferline").get(),
+          })
+        end,
+      },
+      {
+        "nvim-telescope/telescope.nvim",
+        optional = true,
+        opts = {
+          highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+          },
+        },
+      },
+    },
+  }
+}
